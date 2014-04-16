446d821ccb33d56caf697ecb0600ad92e4d1f19b
Pierre Paul <Pierre Paul@130.190.123.13>
2014-04-16 08:21:51 +0200

    Création de la classe Perso et de ses attributs ainsi que son constructeur et de ses accesseurs en lecture.

--- src/rpg/Perso.java
+++ src/rpg/Perso.java
@@ -2,5 +2,43 @@ package rpg;
 
 public class Perso
 {
-
+	private int ptVie;
+	private int ptAttaque;
+	private int ptDefense;
+	private int ptPuissance;
+	private int ptMana;
+	
+	public Perso(int ptVie,int ptAttaque,int ptDefense,int ptPuissance,int ptMana)
+	{
+		this.ptVie = ptVie;
+		this.ptAttaque = ptAttaque;
+		this.ptDefense = ptDefense;
+		this.ptPuissance = ptPuissance;
+		this.ptMana = ptMana;
+	}
+	
+	public int getPtVie()
+	{
+		return this.ptVie;
+	}
+	
+	public int getPtAttaque()
+	{
+		return this.ptAttaque;
+	}
+	
+	public int getPtDefense()
+	{
+		return this.ptDefense;
+	}
+	
+	public int getPtPuissance()
+	{
+		return this.ptPuissance;
+	}
+	
+	public int getPtMana()
+	{
+		return this.ptMana;
+	}
 }
