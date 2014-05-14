caa604203eac685315f28527869d38a15d1b48b1
Junior <Junior@192.168.0.15>
2014-05-14 22:33:08 +0200

    Ajout de Javadoc.

--- src/rpg/Type.java
+++ src/rpg/Type.java
@@ -1,16 +1,53 @@
 package rpg;
 
+import java.util.List;
+
+/**
+ * @author Camille Blaser - Ervan Combe
+ */
 public class Type
 {
-	private NomType nomType;
-	/** Toutes les techniques à la disposition de ce personnage
+	/**
+	 * Le nom du type du personnage.
 	 */
-	private Technique listeTechnique[];
+	private NomType nomType;
+	
+	
+	/** 
+	 * Toutes les techniques à la disposition du personnage.
+	 */
+	private List<Technique> listeTechnique;
 
-	public Type(NomType nom, Technique listeTechnique[])
+	
+	/**
+	 * Créer un nouveau type.
+	 * @param nom du type
+	 * @param listeTechnique techniques utilisées pas le type
+	 */
+	public Type(NomType nom, List<Technique> listeTechnique)
 	{
 		this.nomType = nom;
 		this.listeTechnique = listeTechnique;
+	}
+	
+	
+	/**
+	 * Obtenir le nom du type.
+	 * @return nom du type
+	 */
+	public NomType getNomType()
+	{
+		return this.nomType;
+	}
+	
+	
+	/**
+	 * Obtenir la liste des personnages.
+	 * @return listeTechnique la liste des techniques du personnage
+	 */
+	public List<Technique> getListeTechnique()
+	{
+		return this.listeTechnique;
 	}
 }
 
